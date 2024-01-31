fd99fb58c7daa9e7aec7852a44cf3a9c52422056
Nizvan MR <njr01397@gmail.com>
2024-01-30 19:34:14 -0600

    refactor: making navigation in its own components

---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
+import GeneralStack from "./GeneralStack";
+
+export default function Navigation() {
+    return (
+        <NavigationContainer>
+            <GeneralStack />
+        </NavigationContainer>
+    )
+}
