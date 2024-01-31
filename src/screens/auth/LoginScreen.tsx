032d5ee42d292d38bb103942025ec322bd4dc409
Nizvan Monteón Ricárdez <njr01397@gmail.com>
2024-01-30 20:37:07 -0600

    style: adding style to the login screen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,22 +1,33 @@
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons'
 
 export default function LoginScreen() {
     const navigation = useNavigation();
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>
-                Login screen
-            </Text>
+            <View style={styles.top_card}>
+                <Text style={styles.title}>Klar</Text>
+                <View style={styles.info_card}>
+                    <Text style={styles.text_white}>de Servicio</Text>
+                    <Text style={styles.text_white}>Financieros Alternativos SFP</Text>
+                </View>
+            </View>
 
-            <TouchableOpacity
-                style={styles.button}
-                onPress={() => navigation.navigate('General')}>
-                <Text style={styles.button_text}>
-                    Iniciar con huella dactilar
-                </Text>
-            </TouchableOpacity>
+            <View style={styles.bottom_card}>
+                <Text style={styles.greeting}>Buenas noches, Nizvan</Text>
+                <Text style={styles.is_not_you}>¿No eres tu?</Text>
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={() => navigation.navigate('General')}
+                >
+                    <Ionicons name="finger-print-outline" size={30} color={'white'} />
+                    <Text style={styles.button_text}>
+                        Iniciar con huella dactilar
+                    </Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -26,21 +37,58 @@ const styles = StyleSheet.create({
         flex: 1,
         alignContent: 'center',
         justifyContent: 'space-between',
-        paddingVertical: 40
+        paddingVertical: 40,
+        backgroundColor: '#bbb'
+    },
+    top_card: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        height: 50,
+        alignItems: 'center',
+        gap: 10,
+        marginTop: 60,
+    },
+    info_card: {
+        display: 'flex',
+        height: '100%',
+        justifyContent: 'flex-end',
     },
     title: {
+        fontSize: 56,
+        color: 'white',
+        textAlign: 'center'
+    },
+    text_white: {
+        color: 'white',
+        fontSize: 16
+    },
+    bottom_card: {
+        paddingHorizontal: 12,
+    },
+    greeting: {
+        marginBottom: 8,
+        color: 'white',
         fontSize: 24,
-        textAlign: 'center',
-        marginTop: 60
+        fontWeight: '800'
+    },
+    is_not_you: {
+        marginBottom: 32,
+        color: 'white'
     },
     button: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 4,
         borderRadius: 8,
-        marginHorizontal: 12,
-        paddingVertical: 20,
+        paddingVertical: 14,
         backgroundColor: '#222'
     },
     button_text: {
         color: 'white',
-        textAlign: 'center'
+        textAlign: 'center',
+        fontSize: 16
     }
 });
