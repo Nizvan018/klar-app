b467d2d75347941d3511c2db66cae7bdc17406bc
Nizvan MR <njr01397@gmail.com>
2024-02-13 21:26:14 -0600

    feat: Header, ToggleSwitch and Card component were added

---
+++ assets/styles/main.ts
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+
+export const main = StyleSheet.create({
+    flex: {
+        display: 'flex'
+    },
+    flex_row: {
+        flexDirection: 'row'
+    },
+    space_between: {
+        justifyContent: 'space-between'
+    },
+    align_center: {
+        alignItems: 'center'
+    },
+    p_16: {
+        padding: 16
+    },
+    mt_16: {
+        marginTop: 16
+    },
+    gap_16: {
+        gap: 16
+    },
+    color_primary: {
+        color: '#398'
+    },
+    color_gray: {
+        color: '#555'
+    },
+    bg_primary: {
+        backgroundColor: '#398'
+    },
+    bg_gray: {
+        backgroundColor: '#555'
+    }
+});
---
+++ src/components/header.jsx
@@ -0,0 +1,55 @@
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { AntDesign, Entypo, Ionicons } from '@expo/vector-icons';
+import { useUser } from "@/context/AuthContext";
+import { logout } from "@/api/auth";
+
+export default function Header() {
+    const user = useUser();
+
+    return (
+        <View style={[styles.header, styles.flex, styles.flex_row]}>
+            <TouchableOpacity
+                onPress={logout}
+                style={[styles.profile_icon, styles.flex]}>
+                <Text style={styles.profile_text}>
+                    {user?.email?.charAt(0).toUpperCase()}
+                </Text>
+            </TouchableOpacity>
+            <View style={[styles.flex, styles.flex_row, styles.icons_container]}>
+                <AntDesign name='eyeo' size={24} color={'black'} />
+                <Entypo name='sound' size={24} color={'black'} />
+                <Ionicons name='notifications-outline' size={24} color={'black'} />
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    flex: {
+        display: 'flex'
+    },
+    flex_row: {
+        flexDirection: 'row'
+    },
+    header: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 100,
+        paddingTop: 24
+    },
+    profile_icon: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 40,
+        aspectRatio: 1,
+        borderRadius: 100,
+        backgroundColor: 'white'
+    },
+    profile_text: {
+        fontSize: 18,
+        fontWeight: '700'
+    },
+    icons_container: {
+        gap: 24
+    }
+});
