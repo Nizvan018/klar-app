3f2378f6ca3caff54b0fb02782223adfe26c24fe
Nizvan MR <njr01397@gmail.com>
2024-04-20 13:54:41 -0600

    Feat(Trasnfer View): Validating input

--- src/screens/transfer/TrasnferScreen.tsx
+++ src/screens/transfer/TrasnferScreen.tsx
@@ -3,18 +3,38 @@ import { AntDesign } from '@expo/vector-icons';
 import { main } from "@assets/styles/main"
 import { RouteProp, useNavigation } from "@react-navigation/native";
 import { useUser } from "@/context/AuthContext";
+import { useForm, Controller } from "react-hook-form";
 
 interface Props {
     route: RouteProp<{}>
 }
 
 export default function TrasnferScreen({ route }: Props) {
+    const { control, handleSubmit, formState: { errors } } = useForm();
     const navigation = useNavigation();
     const { contacto } = route.params;
     const { account } = useUser();
 
     const goBack = () => {
         navigation.goBack();
+    }
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data.amount);
+    });
+
+    const handleInputChange = (text: string) => {
+        const correctAmount = text.replace(/^0+(?=\d)/, '');
+        const numeric = correctAmount.replace(/[^0-9.]/g, '');
+
+        const regex = /^(\d+)\.(\d{3})$/;
+        const match = numeric.match(regex);
+
+        if (match) {
+            return numeric.slice(0, -1);
+        } else {
+            return numeric;
+        }
     }
 
     return (
@@ -35,13 +55,45 @@ export default function TrasnferScreen({ route }: Props) {
 
                 <View style={[main.flex, main.align_center]}>
                     <View style={[main.flex, main.flex_row, main.align_center]}>
-                        <Text style={styles.quantity}>$</Text>
-                        <TextInput style={styles.quantity} keyboardType="number-pad" selectionColor={'black'} />
+                        <Text style={[styles.quantity, errors?.amount?.type == 'min' || errors?.amount?.type == 'required' ? styles.bad_quantity : {}]}>$</Text>
+                        <Controller
+                            control={control}
+                            name="amount"
+                            rules={{
+                                required: {
+                                    value: true,
+                                    message: 'Por favor, introduzca la cantidad'
+                                },
+                                min: {
+                                    value: 1,
+                                    message: 'La cantidad mínima es de $1 para transferir'
+                                },
+                                max: {
+                                    value: account.amount,
+                                    message: 'Cantidad de dinero no disponible'
+                                }
+                            }}
+                            render={({ field: { value, onChange, onBlur }, fieldState: { error } }) => (
+                                <>
+                                    <TextInput
+                                        value={value}
+                                        onChangeText={(text) => { onChange(handleInputChange(text)) }}
+                                        onBlur={onBlur}
+                                        style={[styles.quantity, error?.type == 'min' ? styles.bad_quantity : {}]}
+                                        keyboardType="number-pad"
+                                        selectionColor={'black'}
+                                    />
+                                </>
+                            )}
+                        />
                     </View>
-                    <Text>${account.amount.toFixed(2)} disponibles para transferir</Text>
+                    <Text style={errors?.amount?.type == "max" ? styles.bad_quantity : {}}>${account.amount.toFixed(2)} disponibles para transferir</Text>
+                    {errors?.amount?.type == 'min' && (
+                        <Text style={[main.mt_16, styles.bad_quantity]}>{errors.amount.message}</Text>
+                    )}
                 </View>
 
-                <TouchableOpacity style={styles.button} onPress={() => { }}>
+                <TouchableOpacity style={styles.button} onPress={onSubmit}>
                     <Text style={styles.button_text}>Realizar pago</Text>
                 </TouchableOpacity>
             </View>
@@ -73,6 +125,9 @@ const styles = StyleSheet.create({
     },
     quantity: {
         fontSize: 64
+    },
+    bad_quantity: {
+        color: '#f66'
     },
     button: {
         width: '100%',
